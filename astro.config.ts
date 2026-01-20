ec7cda71a037532c5dd74f5a72c479f244b05dc1
rythmofus <rythmofus@gmail.com>
2026-01-20 11:56:17 +0600

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,4 @@
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
-import vercel from '@astrojs/vercel'
 import AstroPureIntegration from 'astro-pure'
 import { defineConfig, fontProviders } from 'astro/config'
 import rehypeKatex from 'rehype-katex'
@@ -25,21 +24,12 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // [Basic]
-  site: 'https://astro-pure.js.org',
-  // Deploy to a sub path
-  // https://astro-pure.js.org/docs/setup/deployment#platform-with-base-path
-  // base: '/astro-pure/',
+  site: 'https://silentevidence.pages.dev',
   trailingSlash: 'never',
-  // root: './my-project-directory',
-  server: { host: true },
 
   // [Adapter]
-  // https://docs.astro.build/en/guides/deploy/
-  adapter: vercel(),
-  output: 'server',
-  // Local (standalone)
-  // adapter: node({ mode: 'standalone' }),
-  // output: 'server',
+  // Cloudflare Pages (static)
+  output: 'static',
 
   // [Assets]
   image: {
@@ -71,8 +61,6 @@ export default defineConfig({
         dark: 'github-dark'
       },
       transformers: [
-        // Two copies of @shikijs/types (one under node_modules
-        // and another nested under @astrojs/markdown-remark → shiki).
         // Official transformers
         // @ts-ignore this happens due to multiple versions of shiki types
         transformerNotationDiff(),
@@ -98,31 +86,18 @@ export default defineConfig({
   // [Integrations]
   integrations: [
     // astro-pure will automatically add sitemap, mdx & unocss
-    // sitemap(),
-    // mdx(),
     AstroPureIntegration(config)
   ],
 
   // [Experimental]
   experimental: {
-    // Allow compatible editors to support intellisense features for content collection entries
-    // https://docs.astro.build/en/reference/experimental-flags/content-intellisense/
     contentIntellisense: true,
-    // Enable SVGO optimization for SVG assets
-    // https://docs.astro.build/en/reference/experimental-flags/svg-optimization/
     svgo: true,
-    // Enable font preloading and optimization
-    // https://docs.astro.build/en/reference/experimental-flags/fonts/
     fonts: [
       {
         provider: fontProviders.fontshare(),
         name: 'Satoshi',
         cssVariable: '--font-satoshi',
-        // Default included:
-        // weights: [400],
-        // styles: ["normal", "italics"],
-        // subsets: ["cyrillic-ext", "cyrillic", "greek-ext", "greek", "vietnamese", "latin-ext", "latin"],
-        // fallbacks: ["sans-serif"],
         styles: ['normal', 'italic'],
         weights: [400, 500],
         subsets: ['latin']
